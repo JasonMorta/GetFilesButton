38b8c49a18de1d2d923f961442a8ca79555e4ef4
JasonMorta <jason.morta23@gmail.com>
2023-01-14 16:05:38 +0200

    updates

--- popup.js
+++ popup.js
@@ -25,3 +25,18 @@ checkbox.addEventListener('click', (e)=>{
 JSON.stringify(localStorage.setItem("loadDbx", e.target.checked))
 chrome.storage.local.set({ openStudentFolder: checkbox.checked });
 })
+
+
+
+/******************************************************************
+
+DropBox API:
+Generate a download link for the task folder.
+
+1. Get the token to give dropbox-app access. Open the access link.
+2. Get the token from the browser URL using the chrome.tabs API
+2. Collect student data from review page
+3. Use data to generate link to task folder.  
+
+*******************************************************************/
+
